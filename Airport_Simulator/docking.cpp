54cedc4e61481fb4454f813c5c773f7468a09b46
Eric Brown <56092687+evvic@users.noreply.github.com>
2020-11-25 17:58:32 +0100

    added a 2nd runway
    one is for departures, one is for landing

--- Airport_Simulator/docking.cpp
+++ Airport_Simulator/docking.cpp
@@ -3,12 +3,7 @@
 #include "Utility.h"
 
 
-/* Program extracts from Chapter 3 of
-   "Data Structures and Program Design in C++"
-   by Robert L. Kruse and Alexander J. Ryba
-   Copyright (C) 1999 by Prentice-Hall, Inc.  All rights reserved.
-   Extracts from this file may be used in the construction of other programs,
-   but this code will not compile or execute as given here. */
+//P2, modified main ot give 2 runways: one for departing and one for arriving
 
 
    // Section 3.1:
@@ -70,7 +65,6 @@ Post: The Queue is initialized to be empty.
     front = 0;
 }
 
-
 bool Queue::empty() const
 /*
 Post: Return true if the Queue is empty, otherwise return false.
@@ -78,7 +72,6 @@ Post: Return true if the Queue is empty, otherwise return false.
 {
     return count == 0;
 }
-
 
 Error_code Queue::append(const Queue_entry& item)
 /*
@@ -94,7 +87,6 @@ return an Error_code of overflow and leave the Queue unchanged.
     return success;
 }
 
-
 Error_code Queue::serve()
 /*
 Post: The front of the Queue is removed. If the Queue
@@ -108,7 +100,6 @@ is empty return an Error_code of underflow.
     return success;
 }
 
-
 Error_code Queue::retrieve(Queue_entry& item) const
 /*
 Post: The front of the Queue retrieved to the output
@@ -120,7 +111,6 @@ Post: The front of the Queue retrieved to the output
     item = entry[front];
     return success;
 }
-
 
 int Extended_queue::size() const
 /*
@@ -457,39 +447,48 @@ Uses: Classes Runway, Plane, Random and functions run_idle, initialize.
     double arrival_rate, departure_rate;
     initialize(end_time, queue_limit, arrival_rate, departure_rate);
     Random variable;
-    Runway small_airport(queue_limit);
+    Runway departing_runway(queue_limit);
+    Runway arriving_runway(queue_limit);
     for (int current_time = 0; current_time < end_time; current_time++) { //  loop over time intervals
 
         int number_arrivals = variable.poisson(arrival_rate);  //  current arrival requests
 
         for (int i = 0; i < number_arrivals; i++) {
             Plane current_plane(flight_number++, current_time, arriving);
-            if (small_airport.can_land(current_plane) != success)
+            if (arriving_runway.can_land(current_plane) != success)
                 current_plane.refuse();
+        }
+
+        Plane moving_plane;
+        switch (arriving_runway.activity(current_time, moving_plane)) {
+            //  Let at most one Plane onto the Runway at current_time.
+        case land:
+            moving_plane.land(current_time);
+            break;
+        case idle:
+            run_idle(current_time);
         }
 
         int number_departures = variable.poisson(departure_rate); //  current departure requests
 
         for (int j = 0; j < number_departures; j++) {
             Plane current_plane(flight_number++, current_time, departing);
-            if (small_airport.can_depart(current_plane) != success)
+            if (departing_runway.can_depart(current_plane) != success)
                 current_plane.refuse();
         }
 
-        Plane moving_plane;
-        switch (small_airport.activity(current_time, moving_plane)) {
+        switch (departing_runway.activity(current_time, moving_plane)) {
             //  Let at most one Plane onto the Runway at current_time.
         case land:
             moving_plane.land(current_time);
-            break;
-        case takeoff_enum:
-            moving_plane.fly(current_time);
             break;
         case idle:
             run_idle(current_time);
         }
     }
-    small_airport.shut_down(end_time);
+    departing_runway.shut_down(end_time);
+    arriving_runway.shut_down(end_time);
+
 }
 
 
